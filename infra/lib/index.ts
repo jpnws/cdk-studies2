9b490a5a007ac8dd1c1fa83474a89f06aa2b0fa0
Ji Park <ji.park@jpnws.com>
2024-03-22 19:38:30 -0700

    Finishing up chapter 2 of AWS CDK in practice.

--- infra/lib/index.ts
+++ infra/lib/index.ts
@@ -1,8 +1,13 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
+import { S3Bucket } from "./constructs/s3bucket";
 
 export class WebStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    // The code that defines your stack goes here.
+    const bucket = new S3Bucket(this, "MyRemovableBucket", {
+      environment: "development",
+    });
   }
 }
